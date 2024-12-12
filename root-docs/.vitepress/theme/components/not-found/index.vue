98be16bafc1957759a4b24e762eae54557d7bfcf
zocv <157280112+zocv@users.noreply.github.com>
2024-12-12 13:41:12 +0800

    1.6.9.
    优化了404页面的样式

--- root-docs/.vitepress/theme/components/not-found/index.vue
+++ root-docs/.vitepress/theme/components/not-found/index.vue
@@ -1,23 +1,103 @@
 <template>
-    <hr>
-
-    <h1 style="font-size: larger;"> ⭐该页面未找到，可能的原因有：</h1>
 
 
-    <hr>
+    <div class="nf-all">
 
-    <br>
+        <div class="nf-tip">
 
-    <ul>
-        <li> ① 该页面是隐私页面，您并未得到授权。</li>
-        <br>
-        <li> ② 该页面属于本地页面，文件并未上传到服务器上，仅能本地阅览。</li>
-        <br>
-        <li> ③ 页面已被删除。</li>
-    </ul>
+            <div class="nf-tip-title">
+                🚫 抱歉，页面错误，可能的原因有：
+            </div>
+
+
+            <div class="nf-tip-content">
+                <ul>
+                    <li> 1️⃣ 路径错误，请检查页面路径。</li>
+                    <br>
+                    <li> 2️⃣ 该页面是隐私页面，您并未获得授权。</li>
+                    <br>
+                    <li> 3️⃣ 该页面属于本地页面，未上传在线服务器，仅能本地阅览。</li>
+                    <br>
+                    <li> 4️⃣ 页面已被管理员删除。</li>
+                </ul>
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+
+
+    <!-- <hr>
+
+
+    -->
 </template>
 
 <script setup>
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.nf-all {
+    min-height: 90vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .nf-tip {
+        width: 50%;
+        height: 50%;
+        z-index: 10;
+
+        .nf-tip-title {
+            height: 80px;
+            width: 800px;
+            text-align: center;
+
+            background-color: white;
+            line-height: 80px;
+            padding: 0 10px;
+            border-radius: 20px;
+            font-size: 30px;
+            font-family: '剑豪体';
+
+            border: 2px dashed rgb(75, 172, 168);
+
+        }
+
+        .nf-tip-content {
+            margin-top: 20px;
+            padding: 20px;
+
+            width: 800px;
+            border: 2px dashed rgb(75, 172, 168);
+
+            background-color: white;
+            line-height: 20px;
+            border-radius: 20px;
+            font-size: 20px;
+            font-family: '楷体';
+        }
+
+    }
+}
+
+.nf-all::after {
+    content: "";
+    background-image: url('/gallery/ai-海浪背景.png');
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    opacity: 0.3;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+
+}
+</style>
--- root-docs/zo-articles/24110501 vitepress-sugarat主题.md
+++ root-docs/zo-articles/24110501 vitepress-sugarat主题.md
@@ -14,18 +14,27 @@ id: 24110501
 
 ::: info <Badge type='info'>介绍</Badge>
 
-**这是基于 `vitepress` 二次开发的博客主题**，是目前我看到的比较**正式、完善**的博客主题。 
+**这是基于 `vitepress` 二次开发的博客主题**，是目前我看到的比较 **正式、完善** 的 vitepress 博客主题。 
 
 :::
+
+![](/articlesPic/202412112131.png)
+
+
+<br/>
+
 
 - **项目官网**：https://theme.sugarat.top/   
 
 👆 `使用教程` 、 `项目介绍` 、`更新公告` 
 
+
+
 - **代码仓库**：https://github.com/ATQQ/sugar-blog <Badge type='danger'>霍格沃茨</Badge>
 
+<br/>
 
-![](/articlesPic/202412112131.png)
+
 
 
 ## 1. 创建项目
